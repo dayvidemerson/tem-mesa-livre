a05ce5a9573d64cf2c1305725f4704be64956db4
Dayvid Emerson <dayvidemerson@rease.com.br>
2019-09-01 18:26:36 -0300

    Fix save e-mail in creation user

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -43,12 +43,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :document_number, :identity, :phone, :kind])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :document_number, :identity, :phone, :email, :kind])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :document_number, :identity, :phone])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :document_number, :identity, :phone, :email])
   end
 
   # The path used after sign up.
